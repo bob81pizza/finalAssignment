a3db40e8c32f8709309565510315d1fc94b8b5f7
unknown <cmartin@Itarget1096.svc.nt.pitt.edu>
2014-07-22 16:37:01 -0400

    Added more tests

--- FinalAssignment.py
+++ FinalAssignment.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
 import unittest
 
 class PythonTesting(unittest.TestCase):
@@ -65,39 +66,39 @@ class PythonTesting(unittest.TestCase):
 	Clear the search box again and search for just numbers. This should open a URL that is
 	different than the first one.
 	'''
-	def testDifferentSearchInputs(self):
-		driver = self.driver
-		driver.get("http://shop.nordstrom.com")
-		searchBox = driver.find_element_by_id("keyword-search-input")
+	# def testDifferentSearchInputs(self):
+	# 	driver = self.driver
+	# 	driver.get("http://shop.nordstrom.com")
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
 
-		#Test numbers and letters
-		searchBox.send_keys("abc123")
-		searchBox.send_keys(Keys.RETURN)
-		self.assertNotEqual(driver.current_url, "http://shop.nordstrom.com")
-		pastURL = driver.current_url
+	# 	#Test numbers and letters
+	# 	searchBox.send_keys("abc123")
+	# 	searchBox.send_keys(Keys.RETURN)
+	# 	self.assertNotEqual(driver.current_url, "http://shop.nordstrom.com")
+	# 	pastURL = driver.current_url
 
-		searchBox = driver.find_element_by_id("keyword-search-input")
-		searchBox.clear()
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	searchBox.clear()
 
-		#Test just special characters
-		searchBox.send_keys("$%*(")
-		searchButton = driver.find_element_by_id("keyword-search-submit")
-		searchButton.click()
+	# 	#Test just special characters
+	# 	searchBox.send_keys("$%*(")
+	# 	searchButton = driver.find_element_by_id("keyword-search-submit")
+	# 	searchButton.click()
 
-		try:
-			WebDriverWait(driver, 3).until(EC.alert_is_present(), 'Timed out')
-			alert = driver.switch_to.alert
-			alert.accept()
-		except TimeoutException:
-			self.fail("No alert is present")
+	# 	try:
+	# 		WebDriverWait(driver, 3).until(EC.alert_is_present(), 'Timed out')
+	# 		alert = driver.switch_to.alert
+	# 		alert.accept()
+	# 	except TimeoutException:
+	# 		self.fail("No alert is present")
 
-		searchBox = driver.find_element_by_id("keyword-search-input")
-		searchBox.clear()	
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	searchBox.clear()	
 
-		#Test just numbers
-		searchBox.send_keys("765")
-		searchBox.send_keys(Keys.RETURN)
-		self.assertNotEqual(driver.current_url, pastURL)
+	# 	#Test just numbers
+	# 	searchBox.send_keys("765")
+	# 	searchBox.send_keys(Keys.RETURN)
+	# 	self.assertNotEqual(driver.current_url, pastURL)
 
 
 	'''
@@ -114,9 +115,68 @@ class PythonTesting(unittest.TestCase):
 	# 	self.assertTrue('no results' in result.text.lower())
 
 
+	'''
+	Navigate to the main page. Search for a specific product number of a suit.
+	Ensure that the product page is directly displayed.
+	'''
+	# def testProductNumber(self):
+	# 	driver = self.driver
+	# 	driver.get("http://shop.nordstrom.com")
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	searchBox.send_keys("712826")
+	# 	searchBox.send_keys(Keys.RETURN)
+	# 	result = driver.find_element_by_xpath("//section[@id='product-title']/h1")
+	# 	self.assertTrue('Milbank' in result.text)
+
+	'''
+	Navigate to the main page. Searh for 'sock.' Results should be returned.
+	Click on the 'x' in the search box. No text should appear in search box.
+	'''
+	# def testClearButton(self):
+	# 	driver = self.driver
+	# 	driver.get("http://shop.nordstrom.com")
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	searchBox.send_keys("sock")
+	# 	searchBox.send_keys(Keys.RETURN)
+
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	clearButton = driver.find_element_by_id("keyword-search-reset")
+	# 	clearButton.click()
+
+	# 	self.assertEqual(searchBox.text, "")
+
+
+	'''
+	Navigate to main page. Check to see that entering 'jea' navigating away and
+	then back, and using the arrow keys will move through the list of lookahead
+	terms.
+	'''
+	# def testSearchLookahead(self):
+	# 	driver = self.driver
+	# 	driver.get("http://shop.nordstrom.com")
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+	# 	searchBox.send_keys("jea")
+
+	# 	driver.find_element_by_xpath("//body").click()
+	# 	searchBox.click()
+
+	# 	searchBox.send_keys(Keys.ARROW_DOWN)
+	# 	searchBox.send_keys(Keys.ARROW_DOWN)
+
+	# 	searchBox.send_keys(Keys.RETURN)
+		
+	# 	searchBox = driver.find_element_by_id("keyword-search-input")
+
+	# 	self.assertTrue("keyword=jeans" in driver.current_url)
+
+
+
+
+
 
 	def tearDown(self):
-		self.driver.quit()
+		# self.driver.quit()
+		pass
 		
 
 if __name__ == "__main__":
